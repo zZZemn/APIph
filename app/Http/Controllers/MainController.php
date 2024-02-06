914f0b981942ad91d59ecad8faaeadfea944aa8a
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-06 20:40:48 +0800

    database

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Region;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        // return view(
+        //     'main',
+        //     [
+        //         'regions' => Region::all()
+        //     ]
+        // );
         return view('main');
     }
 }
