24d52f7f70cb3cfaea14074bb9d38738a6c85b1b
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-07 22:17:01 +0800

    display api endpoints

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,1 @@
+<center class="text-secondary pb-5 pt-5">APIph @ 2024. All rights reserved</center>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,52 @@
 @extends('layout.layout')
 
 @section('content')
-    <h3>Welcome!</h3>
+    <div class="container mt-5">
+        <h3 class="text-secondary">Welcome to APIph!</h3>
+        <div class="endpoints-main-container mt-5 mb-5">
+            <div class="card container p-3">
+                <h6>Get Regions</h6>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/regions">
+                </div>
+            </div>
+            <div class="card container p-3 mt-4">
+                <h6>Get Provinces</h6>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/provinces">
+                </div>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly
+                        value="http://127.0.0.1:8000/api/provinces?region_id=">
+                </div>
+            </div>
+            <div class="card container p-3 mt-4">
+                <h6>Get Municipalities</h6>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/municipalities">
+                </div>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly
+                        value="http://127.0.0.1:8000/api/municipalities?province_id=">
+                </div>
+            </div>
+            <div class="card container p-3 mt-4">
+                <h6>Get Barangays</h6>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/barangays">
+                </div>
+                <div class="endpoint-container d-flex mt-2">
+                    <button class="btn btn-success">GET</button>
+                    <input class="form-control" type="text" readonly
+                        value="http://127.0.0.1:8000/api/barangays?municipality_id=">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
