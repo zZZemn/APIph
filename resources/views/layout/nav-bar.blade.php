60c84320ebd530d59840fc8c547434f53439a3ae
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-07 22:55:20 +0800

    adding logo in nav bar

--- resources/views/layout/nav-bar.blade.php
+++ resources/views/layout/nav-bar.blade.php
@@ -1,5 +1,10 @@
 <nav class="d-flex justify-content-between align-items-center p-5 pt-3 pb-3">
-    <h4 class="text-light">APIph</h4>
+    <div class="d-flex align-items-center">
+        <img src="{{ URL('img/ph-flag.png') }}" alt="Logo" style="height: 32px; width: 32px">
+        <h4 class="text-light m-2 mt-0 mb-0">
+            APIph
+        </h4>
+    </div>
     <div>
         <a href="/" class="text-decoration-none text-secondary m-3 mt-0 mb-0">Home</a>
         <a href="/about" class="text-decoration-none text-secondary m-3 mt-0 mb-0">About</a>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,46 +18,54 @@
                 </p>
         </div>
         <div class="endpoints-main-container mt-5 mb-5">
-            <div class="card container p-3">
+            <div class="card container p-3"
+                style="background-color: rgba(255, 255, 255, 0.014); color: rgba(255, 255, 255, 0.836)">
                 <h6>Get Regions</h6>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/regions">
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                        value="http://127.0.0.1:8000/api/regions">
                 </div>
             </div>
-            <div class="card container p-3 mt-4">
+            <div class="card container p-3 mt-4"
+                style="background-color: rgba(255, 255, 255, 0.014); color: rgba(255, 255, 255, 0.836)">
                 <h6>Get Provinces</h6>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/provinces">
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                        value="http://127.0.0.1:8000/api/provinces">
                 </div>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
                         value="http://127.0.0.1:8000/api/provinces?region_id=">
                 </div>
             </div>
-            <div class="card container p-3 mt-4">
+            <div class="card container p-3 mt-4"
+                style="background-color: rgba(255, 255, 255, 0.014); color: rgba(255, 255, 255, 0.836)">
                 <h6>Get Municipalities</h6>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/municipalities">
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                        value="http://127.0.0.1:8000/api/municipalities">
                 </div>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
                         value="http://127.0.0.1:8000/api/municipalities?province_id=">
                 </div>
             </div>
-            <div class="card container p-3 mt-4">
+            <div class="card container p-3 mt-4"
+                style="background-color: rgba(255, 255, 255, 0.014); color: rgba(255, 255, 255, 0.836)">
                 <h6>Get Barangays</h6>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly value="http://127.0.0.1:8000/api/barangays">
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                        value="http://127.0.0.1:8000/api/barangays">
                 </div>
                 <div class="endpoint-container d-flex mt-2">
                     <button class="btn btn-success">GET</button>
-                    <input class="form-control" type="text" readonly
+                    <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
                         value="http://127.0.0.1:8000/api/barangays?municipality_id=">
                 </div>
             </div>
