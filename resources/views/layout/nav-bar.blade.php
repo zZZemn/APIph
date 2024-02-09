967d121b0cd0c551a2a7516ba938c3c5df9dd630
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-09 22:14:47 +0800

    nav bar

--- resources/views/layout/nav-bar.blade.php
+++ resources/views/layout/nav-bar.blade.php
@@ -1,10 +1,10 @@
 <nav class="top-nav bg-dark d-flex justify-content-between align-items-center p-5 pt-3 pb-3">
-    <div class="d-flex align-items-center">
+    <a class="d-flex align-items-center" href="/">
         <img class="ph-flag" src="{{ URL('img/ph-flag.png') }}" alt="Logo">
         <h4 class="text-light m-2 mt-0 mb-0">
             APIph
         </h4>
-    </div>
+    </a>
     <div>
         <a href="/" class="text-decoration-none text-secondary m-3 mt-0 mb-0">Home</a>
         <a href="/about" class="text-decoration-none text-secondary m-3 mt-0 mb-0">About</a>
