4d0e12bec21fbc15b4c5f03cff06ede3467ebcaf
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-10 18:31:18 +0800

    Adding description to the endpoint

--- resources/views/layout/endpoints-barangay.blade.php
+++ resources/views/layout/endpoints-barangay.blade.php
@@ -1,18 +1,27 @@
 <div class="card container p-3 mt-4">
     <h6>Get Barangays</h6>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/barangays">
+    <div class="card p-3 mt-2">
+        <p>Retrieve all barangays in the Philippines.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/barangays">
+        </div>
     </div>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/barangays?municipality_code=">
+    <div class="card p-3 mt-2">
+        <p>Retrieve all municipalities in the Philippines from specific municipality.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/barangays?municipality_code=">
+        </div>
     </div>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/barangays?search=">
+    <div class="card p-3 mt-2">
+        <p>Search barangays in the Philippines.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/barangays?search=">
+        </div>
     </div>
 </div>
--- resources/views/layout/endpoints-municipality.blade.php
+++ resources/views/layout/endpoints-municipality.blade.php
@@ -1,18 +1,27 @@
 <div class="card container p-3 mt-4">
     <h6>Get Municipalities</h6>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/municipalities">
+    <div class="card p-3 mt-2">
+        <p>Retrieve all municipalities in the Philippines.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/municipalities">
+        </div>
     </div>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/municipalities?province_code=">
+    <div class="card p-3 mt-2">
+        <p>Retrieve all municipalities in the Philippines from specific province.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/municipalities?province_code=">
+        </div>
     </div>
-    <div class="endpoint-container d-flex mt-2">
-        <button class="btn btn-success">GET</button>
-        <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
-            value="http://127.0.0.1:8000/api/municipalities?search=">
+    <div class="card p-3 mt-2">
+        <p>Search municipalities in the Philippines.</p>
+        <div class="endpoint-container d-flex mt-1">
+            <button class="btn btn-success">GET</button>
+            <input class="form-control bg-dark text-light m-1 mt-0 mb-0" type="text" readonly
+                value="http://127.0.0.1:8000/api/municipalities?search=">
+        </div>
     </div>
 </div>
